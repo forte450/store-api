5dea1609bd17e340b85fe3d8fcee46d3753cec88
Swarnim Goyal <kajal.gupta6p@gmail.com>
2025-11-25 17:55:10 +0530

    Third Phase

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,9 @@
 const getAllProducts = async (req, res) => {
     res.status(200).json({ message: 'Get all products' });
 }
+
+const getAllProductsTesting = async (req, res) => {
+    res.status(200).json({ message: 'Get all products testing route' });
+}
+
+module.exports = { getAllProducts, getAllProductsTesting };
