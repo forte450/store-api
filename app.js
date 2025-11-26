4744327b81c3e171c19dc23ed67c10e83e3ee83d
Swarnim Goyal <kajal.gupta6p@gmail.com>
2025-11-26 16:23:38 +0530

    Test commet for app file

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const products_routes = require('./routes/products');
+//Middleware
+app.use("/api/products", products_routes);
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
-    const start = async () => {
-        try {
-            app.listen(PORT, () => {
-                console.log(`Server is running on port ${PORT}`);
-            });
-        } catch (error) {
-            console.error('Error starting the server:', error);
-        }               
-    };
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Error starting the server:', error);
+    }
+};
 
 start();    
